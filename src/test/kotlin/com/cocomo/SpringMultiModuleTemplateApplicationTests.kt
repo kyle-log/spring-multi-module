440bfb4a0ffa7ca189185c4a2dd3157986c3b28e
Kyle <cocomo1316@gmail.com>
2023-11-03 16:43:21 +0900

    first commit

---
+++ src/test/kotlin/com/cocomo/SpringMultiModuleTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cocomo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMultiModuleTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
